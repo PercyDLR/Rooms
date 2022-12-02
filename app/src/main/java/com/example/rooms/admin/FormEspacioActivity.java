a490b74e1500abc37c7357868267ffa23668f060
Percy De La Rosa <a20192265@pucp.edu.pe>
2022-12-02 14:51:17 -0500

    Implementado Obtener imágenes de galeria o de cámara

--- app/src/main/java/com/example/rooms/admin/FormEspacioActivity.java
+++ app/src/main/java/com/example/rooms/admin/FormEspacioActivity.java
@@ -1,10 +1,15 @@
 package com.example.rooms.admin;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +21,8 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 
+import java.io.IOException;
+
 
 public class FormEspacioActivity extends AppCompatActivity {
 
@@ -26,11 +33,21 @@ public class FormEspacioActivity extends AppCompatActivity {
     private EspacioDTO espacio;
     private Boolean conFoto = false;
 
+    private int PERMISOS_CODE = 100;
+    private int GALERIA_CODE = 101;
+    private int CAMARA_CODE = 102;
+    private Bitmap bitmap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_espacio);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Se piden los permisos para la cámara
+        if (checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA},PERMISOS_CODE);
+        }
 
         inputNombre = findViewById(R.id.etNombreFormEspacio);
         inputDescripcion = findViewById(R.id.etDescripcionFormEspacio);
@@ -56,23 +73,54 @@ public class FormEspacioActivity extends AppCompatActivity {
                 .setTitle("Obtener Imagen")
                 .setMessage("Elija de donde quiere obtener su imagen")
                 .setNegativeButton("Galeria",((dialogInterface, i) -> {
-                    obtenerImgGaleria();
+
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    intent.setType("image/*");
+                    startActivityForResult(intent,GALERIA_CODE);
                     dialogInterface.dismiss();
+
                 })).setNeutralButton("Cancelar",((dialogInterface, i) -> {
                     dialogInterface.cancel();
                 })).setPositiveButton("Cámara", ((dialogInterface, i) -> {
-                    obtenerImgCamara();
+
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent,CAMARA_CODE);
                     dialogInterface.dismiss();
+
                 })).show();
     }
 
-    // TODO: Implementar obtener imagen de cámara y de galería
-    private void obtenerImgGaleria(){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if(requestCode == GALERIA_CODE && data != null){
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),data.getData());
+                btnFoto.setImageBitmap(bitmap);
+                conFoto = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == CAMARA_CODE && data != null){
+            bitmap = (Bitmap) data.getExtras().get("data");
+            btnFoto.setImageBitmap(bitmap);
+            conFoto = true;
+        }
     }
 
-    private void obtenerImgCamara() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        if(requestCode==PERMISOS_CODE && grantResults.length > 0){
+            if (grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                // Se piden los permisos nuevamente
+                if (checkSelfPermission(Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},PERMISOS_CODE);
+                }
+            }
+        }
     }
 
     @Override
